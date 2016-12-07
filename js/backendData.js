f38f13972ae3648501d20be07f19a4b54567c6ba
Janine Jay <janinerjay@aol.com>
2016-12-06 22:17:02 -0500

    Update Part One
    Half of my editing done

---
+++ js/backendData.js
@@ -0,0 +1,37 @@
+var chosenPatient = false;
+
+function patient(patNum){
+    if(patNum==1){
+        document.getElementById("allergy").innerHTML = 'Peanuts';
+        document.getElementById("height").innerHTML = '67 inches';
+        document.getElementById("weight").innerHTML = '115 pounds';
+        document.getElementById("age").innerHTML = '56 years and 10 months';
+        chosenPatient = true;
+    }
+    if(patNum==2){
+        document.getElementById("allergy").innerHTML = 'none';
+        document.getElementById("height").innerHTML = '5 foot 7 inches';
+        document.getElementById("weight").innerHTML = '130 pounds';
+        document.getElementById("age").innerHTML = '34 years and 5 months';
+        chosenPatient = true;
+    }
+    if(patNum==3){
+        document.getElementById("allergy").innerHTML = 'Pollen, grass, air';
+        document.getElementById("height").innerHTML = '5 foot 4 inches';
+        document.getElementById("weight").innerHTML = '125 pounds';
+        document.getElementById("age").innerHTML = '14 years and 7 months';
+        chosenPatient = true;
+    }
+}
+
+function medicationChoice(choice){
+    var medChoice = '';
+    if (choice==1){
+        medChoice = 'Humalog';
+        document.getElementById("medication").innerHTML = medChoice;
+    }
+    if (choice==2){
+        medChoice = 'Nomalog';
+        document.getElementById("medication").innerHTML = medChoice;
+    }
+}
---
+++ js/test.js
@@ -0,0 +1,21 @@
+function patient1(){
+    alert('test1');
+    document.getElementById("allergy").innerHTML = 'Peanuts';
+    document.getElementById("height").innerHTML = '5 foot 4 inches;
+    document.getElementById("weight").innerHTML = '115 pounds';
+    document.getElementById("age").innerHTML = '56 years and 10 months';
+}
+
+function patient2(){
+    document.getElementById("allergy").innerHTML = 'none';
+    document.getElementById("height").innerHTML = '5 foot 7 inches';
+    document.getElementById("weight").innerHTML = '130 pounds';
+    document.getElementById("age").innerHTML = '34 years and 5 months';
+}
+
+function patient3(){
+    document.getElementById("allergy").innerHTML = 'Pollen, grass, air';
+    document.getElementById("height").innerHTML = '5 foot 4 inches';
+    document.getElementById("weight").innerHTML = '125 pounds';
+    document.getElementById("age").innerHTML = '14 years and 7 months';
+}
